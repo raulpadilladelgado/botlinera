579735e01ced1a06fbeeea9882daae6e18564bee
iaimans <isaac@leanmind.es>
2022-04-22 17:34:49 +0100

    :bug: Retrieve gas station info as JSON

--- app/src/main/kotlin/botlinera/infrastructure/utils/URLWrapper.kt
+++ app/src/main/kotlin/botlinera/infrastructure/utils/URLWrapper.kt
@@ -3,7 +3,13 @@ package botlinera.infrastructure.utils
 import java.net.URL
 
 class URLWrapper {
-    fun get(url: String): String{
-        return URL(url).readText()
+
+    fun get(url: String): String {
+        val connection =  URL(url).openConnection()
+        connection.setRequestProperty("Accept", "application/json")
+        val response = connection.getInputStream().use {
+            it.readBytes()
+        }.toString(Charsets.UTF_8)
+        return response
     }
 }
