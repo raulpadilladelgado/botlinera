78b13f84a0b811498353e4ed8edb7064a4fd85db
Raúl Padilla Delgado <rpd27@outlook.com>
2022-06-01 20:05:30 +0100

    :construction: add buttons to configurate distance and gas type

--- app/src/main/kotlin/botlinera/infrastructure/bot/TelegramBot.kt
+++ app/src/main/kotlin/botlinera/infrastructure/bot/TelegramBot.kt
@@ -6,16 +6,29 @@ import botlinera.domain.valueobject.GasStation
 import botlinera.domain.valueobject.GasType
 import botlinera.domain.valueobject.GasType.*
 import botlinera.infrastructure.adapters.GastStationPersisterMongo
-import botlinera.infrastructure.utils.BotMessages
+import botlinera.infrastructure.utils.BotMessages.Companion.changeGasTypeButtonText
+import botlinera.infrastructure.utils.BotMessages.Companion.changeMaximumDistanceButtonText
+import botlinera.infrastructure.utils.BotMessages.Companion.chosenDistance
+import botlinera.infrastructure.utils.BotMessages.Companion.chosenGasType
+import botlinera.infrastructure.utils.BotMessages.Companion.findCheapestGasStationsButtonText
+import botlinera.infrastructure.utils.BotMessages.Companion.notGasStationsFound
+import botlinera.infrastructure.utils.BotMessages.Companion.showingGasStations
+import botlinera.infrastructure.utils.BotMessages.Companion.unknowMessage
+import botlinera.infrastructure.utils.BotMessages.Companion.welcoming
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.command
 import com.github.kotlintelegrambot.dispatcher.location
 import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId.Companion.fromId
+import com.github.kotlintelegrambot.entities.KeyboardReplyMarkup
 import com.github.kotlintelegrambot.entities.ParseMode.MARKDOWN
 import com.github.kotlintelegrambot.entities.keyboard.KeyboardButton
 import java.lang.System.getenv
+
+private const val START_COMMAND = "start"
+
+private const val HELP_COMMAND = "ayuda"
 
 class TelegramBot {
     fun startPolling() {
@@ -25,18 +38,20 @@ class TelegramBot {
     private fun initBot() = bot {
         token = getenv("TELEGRAM_BOT_TOKEN")
         var gasType = GASOLINA_95_E5
+        var gasTypePrintable = gasType.printableName
         var maximumDistanceInMeters = 5000
+        val maximumDistanceInKilometers = maximumDistanceInMeters / 1000
         dispatch {
             text {
                 if (text !in commands) {
                     bot.sendMessage(
                         fromId(message.chat.id),
-                        text = BotMessages.unknowMessage(gasType, maximumDistanceInMeters),
+                        text = unknowMessage(gasTypePrintable, maximumDistanceInKilometers),
                         MARKDOWN
                     )
                 }
             }
-            KeyboardButton("Pasame tu location ahí", requestLocation = true)
+
             location {
                 val nearGasStations = getNearGasStations(
                     location.latitude,
@@ -46,11 +61,11 @@ class TelegramBot {
                 )
                 if (nearGasStations.isEmpty()) {
                     bot.sendMessage(
-                        fromId(message.chat.id), text = BotMessages.notGasStationsFound(gasType, maximumDistanceInMeters)
+                        fromId(message.chat.id), text = notGasStationsFound(gasTypePrintable, maximumDistanceInKilometers)
                     )
                 } else {
                     bot.sendMessage(
-                        fromId(message.chat.id), text = BotMessages.showingGasStations(gasType, maximumDistanceInMeters)
+                        fromId(message.chat.id), text = showingGasStations(gasTypePrintable, maximumDistanceInKilometers)
                     )
                     nearGasStations
                         .forEach { gasStation ->
@@ -65,112 +80,152 @@ class TelegramBot {
                         }
                 }
             }
-            command("start") {
-                gasType = GASOLINA_95_E5
+            command(START_COMMAND) {
+                val keyboardButtons = keyboardButtons(gasTypePrintable, maximumDistanceInKilometers)
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.welcoming(gasType, maximumDistanceInMeters),
-                    MARKDOWN
+                    text = welcoming(gasTypePrintable, maximumDistanceInKilometers),
+                    MARKDOWN,
+                    replyMarkup = KeyboardReplyMarkup(keyboardButtons, resizeKeyboard = true)
                 )
             }
-            command("ayuda") {
-                gasType = GASOLINA_95_E5
+            command(HELP_COMMAND) {
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.welcoming(gasType, maximumDistanceInMeters),
+                    text = welcoming(gasTypePrintable, maximumDistanceInKilometers),
                     MARKDOWN
                 )
             }
             command("gasolina_95_e5") {
                 gasType = GASOLINA_95_E5
+                gasTypePrintable = gasType.printableName
+                val keyboardButtons = keyboardButtons(gasTypePrintable, maximumDistanceInKilometers)
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.chosenGasType(gasType),
-                    MARKDOWN
+                    text = chosenGasType(gasTypePrintable),
+                    MARKDOWN,
+                    replyMarkup = KeyboardReplyMarkup(keyboardButtons, resizeKeyboard = true)
                 )
             }
             command("gasolina_95_e5_premium") {
                 gasType = GASOLINA_95_E5_PREMIUM
+                gasTypePrintable = gasType.printableName
+                val keyboardButtons = keyboardButtons(gasTypePrintable, maximumDistanceInKilometers)
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.chosenGasType(gasType),
-                    MARKDOWN
+                    text = chosenGasType(gasTypePrintable),
+                    MARKDOWN,
+                    replyMarkup = KeyboardReplyMarkup(keyboardButtons, resizeKeyboard = true)
                 )
             }
             command("gasolina_95_e10") {
                 gasType = GASOLINA_95_E10
+                gasTypePrintable = gasType.printableName
+                val keyboardButtons = keyboardButtons(gasTypePrintable, maximumDistanceInKilometers)
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.chosenGasType(gasType),
-                    MARKDOWN
+                    text = chosenGasType(gasTypePrintable),
+                    MARKDOWN,
+                    replyMarkup = KeyboardReplyMarkup(keyboardButtons, resizeKeyboard = true)
                 )
             }
             command("gasolina_98_e5") {
                 gasType = GASOLINA_98_E5
+                gasTypePrintable = gasType.printableName
+                val keyboardButtons = keyboardButtons(gasTypePrintable, maximumDistanceInKilometers)
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.chosenGasType(gasType),
-                    MARKDOWN
+                    text = chosenGasType(gasTypePrintable),
+                    MARKDOWN,
+                    replyMarkup = KeyboardReplyMarkup(keyboardButtons, resizeKeyboard = true)
                 )
             }
             command("gasolina_98_e10") {
                 gasType = GASOLINA_98_E10
+                gasTypePrintable = gasType.printableName
+                val keyboardButtons = keyboardButtons(gasTypePrintable, maximumDistanceInKilometers)
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.chosenGasType(gasType),
-                    MARKDOWN
+                    text = chosenGasType(gasTypePrintable),
+                    MARKDOWN,
+                    replyMarkup = KeyboardReplyMarkup(keyboardButtons, resizeKeyboard = true)
                 )
             }
             command("gasoil_a") {
                 gasType = GASOIL_A
+                gasTypePrintable = gasType.printableName
+                val keyboardButtons = keyboardButtons(gasTypePrintable, maximumDistanceInKilometers)
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.chosenGasType(gasType),
-                    MARKDOWN
+                    text = chosenGasType(gasTypePrintable),
+                    MARKDOWN,
+                    replyMarkup = KeyboardReplyMarkup(keyboardButtons, resizeKeyboard = true)
                 )
             }
             command("gasoil_b") {
                 gasType = GASOIL_B
+                gasTypePrintable = gasType.printableName
+                val keyboardButtons = keyboardButtons(gasTypePrintable, maximumDistanceInKilometers)
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.chosenGasType(gasType),
-                    MARKDOWN
+                    text = chosenGasType(gasTypePrintable),
+                    MARKDOWN,
+                    replyMarkup = KeyboardReplyMarkup(keyboardButtons, resizeKeyboard = true)
                 )
             }
             command("gasoil_premium") {
                 gasType = GASOIL_PREMIUM
+                gasTypePrintable = gasType.printableName
+                val keyboardButtons = keyboardButtons(gasTypePrintable, maximumDistanceInKilometers)
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.chosenGasType(gasType),
-                    MARKDOWN
+                    text = chosenGasType(gasTypePrintable),
+                    MARKDOWN,
+                    replyMarkup = KeyboardReplyMarkup(keyboardButtons, resizeKeyboard = true)
                 )
             }
             command("distancia_5") {
                 maximumDistanceInMeters = 5000
+                val keyboardButtons = keyboardButtons(gasTypePrintable, maximumDistanceInKilometers)
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.chosenDistance(maximumDistanceInMeters),
-                    MARKDOWN
+                    text = chosenDistance(maximumDistanceInKilometers),
+                    MARKDOWN,
+                    replyMarkup = KeyboardReplyMarkup(keyboardButtons, resizeKeyboard = true)
                 )
             }
             command("distancia_10") {
                 maximumDistanceInMeters = 10000
+                val keyboardButtons = keyboardButtons(gasTypePrintable, maximumDistanceInKilometers)
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.chosenDistance(maximumDistanceInMeters),
-                    MARKDOWN
+                    text = chosenDistance(maximumDistanceInKilometers),
+                    MARKDOWN,
+                    replyMarkup = KeyboardReplyMarkup(keyboardButtons, resizeKeyboard = true)
                 )
             }
             command("distancia_20") {
                 maximumDistanceInMeters = 20000
+                val keyboardButtons = keyboardButtons(gasTypePrintable, maximumDistanceInKilometers)
                 bot.sendMessage(
                     fromId(message.chat.id),
-                    text = BotMessages.chosenDistance(maximumDistanceInMeters),
-                    MARKDOWN
+                    text = chosenDistance(maximumDistanceInKilometers),
+                    MARKDOWN,
+                    replyMarkup = KeyboardReplyMarkup(keyboardButtons, resizeKeyboard = true)
                 )
             }
         }
     }.startPolling()
+
+    private fun keyboardButtons(gasTypePrintable: String, maximumDistanceInKilometers: Int): List<List<KeyboardButton>> {
+        return listOf(
+            listOf(KeyboardButton(findCheapestGasStationsButtonText(), requestLocation = true)),
+            listOf(
+                KeyboardButton(changeGasTypeButtonText(gasTypePrintable)),
+                KeyboardButton(changeMaximumDistanceButtonText(maximumDistanceInKilometers))
+            )
+        )
+    }
 
     private fun getNearGasStations(latitude: Float, longitude: Float, maximumDistanceInMeters: Int, gasType: GasType): List<GasStation> {
         val gasStationPersister = GastStationPersisterMongo(getenv("DATABASE_URL"))
@@ -182,8 +237,8 @@ class TelegramBot {
     }
 
     private val commands = listOf(
-        "/start",
-        "/ayuda",
+        "/$START_COMMAND",
+        "/$HELP_COMMAND",
         "/gasolina_95_e5",
         "/gasolina_95_e5_premium",
         "/gasolina_95_e10",
--- app/src/main/kotlin/botlinera/infrastructure/utils/BotMessages.kt
+++ app/src/main/kotlin/botlinera/infrastructure/utils/BotMessages.kt
@@ -4,13 +4,13 @@ import botlinera.domain.valueobject.GasType
 
 class BotMessages {
     companion object {
-        fun welcoming(gasType: GasType, maximumDistanceInMeters: Int): String {
+        fun welcoming(actualGasType: String, actualDistance: Int): String {
             return """
                         ¡Hola! Me llamo *Botlinera* 🤖⛽️ y estoy aquí para ayudarte a ahorrar dinero 💸.
                         
                         Para empezar solo tienes que enviarme tu ubicación para así mostrarte las *tres gasolineras más baratas* cerca de ti. 
                         
-                        Actualmente se te muestran los resultados priorizando el combustible ${gasType.printableName} y con una distancia máxima aproximada de ${maximumDistanceInMeters / 1000} kilómetros. Recuerda que puedes cambiar estos ajustes en cualquier momento usando el menú que se encuentra a la izquierda del cuadro para introducir texto o pulsando sobre los links que te dejo aquí abajo:
+                        Actualmente se te muestran los resultados priorizando el combustible $actualGasType y con una distancia máxima aproximada de $actualDistance kilómetros. Recuerda que puedes cambiar estos ajustes en cualquier momento usando el menú que se encuentra a la izquierda del cuadro para introducir texto o pulsando sobre los links que te dejo aquí abajo:
                         /ayuda -> Muestra la ayuda
                         /gasolina_95_e5 -> Gasolina 95 E5
                         /gasolina_95_e5_premium -> Gasolina 95 E5 Premium
@@ -22,38 +22,50 @@ class BotMessages {
                         /gasoil_premium -> Gasoil Premium
                    """.trimIndent()
         }
-        fun unknowMessage(gasType: GasType, maximumDistanceInMeters: Int): String {
+        fun unknowMessage(actualGasType: String, actualDistance: Int): String {
             return """
-                        Lo siento, no entiendo lo que has escrito. Recuerda que puedes mandarme tu ubicación y te mostraré las gasolineras cercanas más baratas en función del combustible que has seleccionado (${gasType.printableName}) y la distancia máxima aproximada de ${maximumDistanceInMeters / 1000} kilómetros
+                        Lo siento, no entiendo lo que has escrito. Recuerda que puedes mandarme tu ubicación y te mostraré las gasolineras cercanas más baratas en función del combustible que has seleccionado ($actualGasType) y la distancia máxima aproximada de $actualDistance kilómetros
                         
                         Si necesitas ayuda usa el comando /help. Puedes pulsar sobre el comando anterior o elegirlo en el menú que hay a la izquierda del cuadro para introducir texto.
                    """.trimIndent()
         }
-        fun notGasStationsFound(gasType: GasType, maximumDistanceInMeters: Int): String {
+        fun notGasStationsFound(actualGasType: String, actualDistance: Int): String {
             return """
-                            Lo siento, no he podido encontrar gasolineras que vendan ${gasType.printableName} en una distancia máxima aproximada de ${maximumDistanceInMeters / 1000} kilómetros
+                            Lo siento, no he podido encontrar gasolineras que vendan $actualGasType en una distancia máxima aproximada de $actualDistance kilómetros
                             
                             Intentalo en otro momento o contacta con los que me programaron 
                             @RaulPadillaDelgado (https://raulpadilladelgado.github.io/)
                             Isaac Aiman ()
                    """.trimIndent()
         }
-        fun chosenGasType(gasType: GasType): String {
+        fun chosenGasType(actualGasType: String): String {
             return """
-                        Quieres echar ${gasType.printableName}. Ahora manda tu ubicación y te buscaré las tres más baratas cerca de ti
+                        Quieres echar $actualGasType. Ahora manda tu ubicación y te buscaré las tres más baratas cerca de ti
                    """.trimIndent()
         }
 
-        fun showingGasStations(gasType: GasType, maximumDistanceInMeters: Int): String {
+        fun showingGasStations(actualGasType: String, actualDistance: Int): String {
             return """
-                        Mostrando las gasolineras más baratas a una distancia máxima de ${maximumDistanceInMeters / 1000} kilómetros para el combustible elegido (${gasType.printableName})
+                        Mostrando las gasolineras más baratas a una distancia máxima de $actualDistance kilómetros para el combustible elegido ($actualGasType)
                    """.trimIndent()
         }
 
-        fun chosenDistance(maximumDistanceInMeters: Int): String {
+        fun chosenDistance(actualDistance: Int): String {
             return """
-                        Se ha establecido ${maximumDistanceInMeters / 1000} kilómetros como la distancia máxima aproximada para la búsqueda de gasolineras
+                        Se ha establecido $actualDistance kilómetros como la distancia máxima aproximada para la búsqueda de gasolineras
                    """.trimIndent()
+        }
+
+        fun findCheapestGasStationsButtonText() :String {
+            return "\uD83D\uDD0E Buscar gasolineras baratas"
+        }
+
+        fun changeGasTypeButtonText(actualGasType: String) :String {
+            return "⛽ Cambiar combustible\n$actualGasType"
+        }
+
+        fun changeMaximumDistanceButtonText(actualDistance: Int) :String {
+            return "\uD83D\uDEE3️ Cambiar distancia\n$actualDistance KMs"
         }
     }
 }
