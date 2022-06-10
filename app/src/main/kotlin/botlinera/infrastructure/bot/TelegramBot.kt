59ddded1d598c399dacaed22b8d2601296b3a940
iaimans <isaac@leanmind.es>
2022-06-10 17:04:25 +0100

    :sparkles: Add all types of gas and new bot flow

--- app/src/main/kotlin/botlinera/infrastructure/bot/TelegramBot.kt
+++ app/src/main/kotlin/botlinera/infrastructure/bot/TelegramBot.kt
@@ -4,21 +4,27 @@ import botlinera.application.usecases.NearGasStation
 import botlinera.domain.valueobject.Coordinates
 import botlinera.domain.valueobject.GasStation
 import botlinera.domain.valueobject.GasType
-import botlinera.domain.valueobject.GasType.GASOLINA_95_E10
-import botlinera.domain.valueobject.GasType.GASOLINA_95_E5
+import botlinera.domain.valueobject.GasType.*
 import botlinera.infrastructure.adapters.GastStationPersisterMongo
-import botlinera.infrastructure.adapters.UserConfigurationRepositoryInMemory
+import botlinera.infrastructure.utils.BotMessages.Companion.chooseGas
 import botlinera.infrastructure.utils.BotMessages.Companion.findCheapestGasStationsButtonText
 import botlinera.infrastructure.utils.BotMessages.Companion.notGasStationsFound
 import botlinera.infrastructure.utils.BotMessages.Companion.showingGasStations
+import botlinera.infrastructure.utils.BotMessages.Companion.unknownMessage
 import botlinera.infrastructure.utils.BotMessages.Companion.welcoming
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.callbackQuery
 import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.handlers.CallbackQueryHandlerEnvironment
+import com.github.kotlintelegrambot.dispatcher.handlers.CommandHandlerEnvironment
+import com.github.kotlintelegrambot.dispatcher.handlers.LocationHandlerEnvironment
+import com.github.kotlintelegrambot.dispatcher.handlers.TextHandlerEnvironment
 import com.github.kotlintelegrambot.dispatcher.location
+import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId.Companion.fromId
 import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
+import com.github.kotlintelegrambot.entities.KeyboardReplyMarkup
 import com.github.kotlintelegrambot.entities.ParseMode.MARKDOWN
 import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
 import com.github.kotlintelegrambot.entities.keyboard.KeyboardButton
@@ -40,83 +46,91 @@ class TelegramBot {
     private fun initBot() = bot {
         token = getenv("TELEGRAM_BOT_TOKEN")
         dispatch {
-             callbackQuery {
-                 val message = callbackQuery.message
-                 val location = message!!.replyToMessage!!.location
-                 val latitude = location!!.latitude
-                 val longitude = location.longitude
-                 val gasType = GasType.valueOf(callbackQuery.data)
-                 val nearGasStations = getNearGasStations(
-                    latitude,
-                    longitude,
-                    gasType = gasType
-                )
-                if (nearGasStations.isEmpty()) {
-                    bot.sendMessage(
-                        fromId(message.chat.id),
-                        text = notGasStationsFound(gasType)
-                    )
-                } else {
-                    bot.sendMessage(
-                        fromId(message.chat.id),
-                        text = showingGasStations(gasType)
-                    )
-                    nearGasStations
-                        .forEach { gasStation ->
-                            bot.sendMessage(
-                                fromId(message.chat.id), text = gasStation.formatted()
-                            )
-                            bot.sendLocation(
-                                fromId(message.chat.id),
-                                gasStation.latitude().toFloat(),
-                                gasStation.longitude().toFloat()
-                            )
-                        }
-                }
-            }
-
-            location {
-                bot.sendMessage(
-                        fromId(message.chat.id),
-                        "Elige tu gasolina",
-                        replyToMessageId = message.messageId,
-                        replyMarkup = InlineKeyboardMarkup.create(
-                                listOf(
-                                        InlineKeyboardButton.CallbackData("95 E5", GASOLINA_95_E5.name),
-                                        InlineKeyboardButton.CallbackData("95 E10", GASOLINA_95_E10.name)
-                                )
-                        )
-                )
-            }
             command(START_COMMAND) {
-                val userConfiguration = UserConfigurationRepositoryInMemory().retrieve(message.chat.id)
-                bot.sendMessage(
-                    fromId(message.chat.id),
-                    text = welcoming(
-                        userConfiguration.gasTypePrintable(),
-                        userConfiguration.maximumDistanceInKilometers()
-                    ),
-                    MARKDOWN
-                )
+                sendHelpMessage()
             }
             command(HELP_COMMAND) {
-                val userConfiguration = UserConfigurationRepositoryInMemory().retrieve(message.chat.id)
-                bot.sendMessage(
-                    fromId(message.chat.id),
-                    text = welcoming(
-                        userConfiguration.gasTypePrintable(),
-                        userConfiguration.maximumDistanceInKilometers()
-                    ),
-                    MARKDOWN
-                )
+                sendHelpMessage()
             }
-            command("buscar") {
-
+            location {
+                handleLocation()
+            }
+            callbackQuery {
+                handleGasStationRequest()
+            }
+            text {
+                sendUnknownMessage()
             }
         }
     }.startPolling()
 
-    private fun keyboardButtons(
+    private fun CommandHandlerEnvironment.sendHelpMessage() {
+        bot.sendMessage(
+            fromId(message.chat.id),
+            text = welcoming(),
+            MARKDOWN,
+            replyMarkup = KeyboardReplyMarkup(locationButton(), resizeKeyboard = true)
+        )
+    }
+
+    private fun LocationHandlerEnvironment.handleLocation() {
+        bot.sendMessage(
+            fromId(message.chat.id),
+            chooseGas(),
+            replyToMessageId = message.messageId,
+            replyMarkup = InlineKeyboardMarkup.create(
+                listOf(
+                    InlineKeyboardButton.CallbackData(GASOLINA_95_E5.printableName,  GASOLINA_95_E5.name),
+                    InlineKeyboardButton.CallbackData(GASOLINA_95_E5_PREMIUM.printableName, GASOLINA_95_E5_PREMIUM.name),
+                    InlineKeyboardButton.CallbackData(GASOLINA_95_E10.printableName, GASOLINA_95_E10.name),
+                    InlineKeyboardButton.CallbackData(GASOLINA_98_E5.printableName, GASOLINA_98_E5.name),
+                    InlineKeyboardButton.CallbackData(GASOLINA_98_E10.printableName, GASOLINA_98_E10.name),
+                    InlineKeyboardButton.CallbackData(GASOIL_A.printableName, GASOIL_A.name),
+                    InlineKeyboardButton.CallbackData(GASOIL_B.printableName, GASOIL_B.name),
+                    InlineKeyboardButton.CallbackData(GASOIL_PREMIUM.printableName, GASOIL_PREMIUM.name),
+
+                )
+            )
+        )
+    }
+
+    private fun CallbackQueryHandlerEnvironment.handleGasStationRequest() {
+        val message = callbackQuery.message
+        val location = message!!.replyToMessage!!.location
+        val latitude = location!!.latitude
+        val longitude = location.longitude
+        val gasType = valueOf(callbackQuery.data)
+        val nearGasStations = getNearGasStations(
+            latitude,
+            longitude,
+            gasType = gasType
+        )
+
+        if (nearGasStations.isEmpty()) {
+            bot.sendMessage(
+                fromId(message.chat.id),
+                text = notGasStationsFound(gasType)
+            )
+        } else {
+            bot.sendMessage(
+                fromId(message.chat.id),
+                text = showingGasStations(gasType)
+            )
+            nearGasStations
+                .forEach { gasStation ->
+                    bot.sendMessage(
+                        fromId(message.chat.id), text = gasStation.formatted()
+                    )
+                    bot.sendLocation(
+                        fromId(message.chat.id),
+                        gasStation.latitude().toFloat(),
+                        gasStation.longitude().toFloat()
+                    )
+                }
+        }
+    }
+
+    private fun locationButton(
     ): List<List<KeyboardButton>> {
         return listOf(
             listOf(KeyboardButton(findCheapestGasStationsButtonText(), requestLocation = true))
@@ -124,16 +138,26 @@ class TelegramBot {
     }
 
     private fun getNearGasStations(
-            latitude: Float,
-            longitude: Float,
-            maximumDistanceInMeters: Int = DEFAULT_DISTANCE_IN_METERS,
-            gasType: GasType = DEFAULT_GAS_TYPE
+        latitude: Float,
+        longitude: Float,
+        maximumDistanceInMeters: Int = DEFAULT_DISTANCE_IN_METERS,
+        gasType: GasType = DEFAULT_GAS_TYPE
     ): List<GasStation> {
         val gasStationPersister = GastStationPersisterMongo(getenv("DATABASE_URL"))
         return NearGasStation(gasStationPersister).execute(
             Coordinates(latitude.toDouble(), longitude.toDouble()),
             maximumDistanceInMeters,
             gasType
+        )
+    }
+
+
+    private fun TextHandlerEnvironment.sendUnknownMessage() {
+        bot.sendMessage(
+            fromId(message.chat.id),
+            text = unknownMessage(),
+            MARKDOWN,
+            replyMarkup = KeyboardReplyMarkup(locationButton(), resizeKeyboard = true)
         )
     }
 }
--- app/src/main/kotlin/botlinera/infrastructure/utils/BotMessages.kt
+++ app/src/main/kotlin/botlinera/infrastructure/utils/BotMessages.kt
@@ -1,96 +1,47 @@
 package botlinera.infrastructure.utils
 
 import botlinera.domain.valueobject.GasType
-import botlinera.domain.valueobject.UserConfiguration
 
 class BotMessages {
     companion object {
-        val gasTypesCommands = listOf(
-            "/gasolina95e5",
-            "/gasolina95e5premium",
-            "/gasolina95e10",
-            "/gasolina98e5",
-            "/gasolina98e10",
-            "/gasoila",
-            "/gasoilb",
-            "/gasoilpremium"
-        )
+        fun welcoming(): String {
+            return """
+                    ¡Hola! Me llamo *Botlinera* 🤖⛽️ y estoy aquí para ayudarte a ahorrar dinero 💸.
+                    
+                    Para empezar solo tienes que enviarme tu ubicación para así mostrarte las *tres gasolineras más baratas* cerca de ti. 
+                   """.trimIndent()
+        }
 
-        fun welcoming(actualGasType: String, actualDistance: Int): String {
+        fun unknownMessage(): String {
             return """
-                        ¡Hola! Me llamo *Botlinera* 🤖⛽️ y estoy aquí para ayudarte a ahorrar dinero 💸.
-                        
-                        Para empezar solo tienes que enviarme tu ubicación para así mostrarte las *tres gasolineras más baratas* cerca de ti. 
-                        
-                        Actualmente se te muestran los resultados priorizando el combustible $actualGasType y con una distancia máxima aproximada de $actualDistance kilómetros. Recuerda que puedes cambiar estos ajustes en cualquier momento usando el menú que se encuentra a la izquierda del cuadro para introducir texto o pulsando sobre los links que te dejo aquí abajo:
-                        /ayuda -> Muestra la ayuda
-                        /gasolina_95_e5 -> Gasolina 95 E5
-                        /gasolina_95_e5_premium -> Gasolina 95 E5 Premium
-                        /gasolina_95_e10 -> Gasolina 95 E10
-                        /gasolina_98_e5 -> Gasolina 98 E5
-                        /gasolina_98_e10 -> Gasolina 98 E10
-                        /gasoil_a -> Gasoil A
-                        /gasoil_b -> Gasoil B
-                        /gasoil_premium -> Gasoil Premium
+                        Lo siento, no entiendo lo que has escrito. Recuerda que puedes mandarme tu ubicación y te mostraré las gasolineras cercanas más baratas.
+                                                
+                        Si necesitas ayuda usa el comando /ayuda. Puedes pulsar sobre el comando anterior o elegirlo en el menú que hay a la izquierda del cuadro para introducir texto.
                    """.trimIndent()
         }
-        fun unknowMessage(userConfiguration: UserConfiguration): String {
-            return """
-                        Lo siento, no entiendo lo que has escrito. Recuerda que puedes mandarme tu ubicación y te mostraré las gasolineras cercanas más baratas en función del combustible que has seleccionado (${userConfiguration.gasTypePrintable()}) y la distancia máxima aproximada de ${userConfiguration.maximumDistanceInKilometers()} kilómetros
-                        
-                        Si necesitas ayuda usa el comando /help. Puedes pulsar sobre el comando anterior o elegirlo en el menú que hay a la izquierda del cuadro para introducir texto.
-                   """.trimIndent()
-        }
+
         fun notGasStationsFound(gasType: GasType): String {
             return """
-                            Lo siento, no he podido encontrar gasolineras que vendan ${gasType.printableName} en una distancia máxima aproximada de 5 kilómetros
+                            Lo siento, no he podido encontrar gasolineras que vendan ${gasType.printableName} en un radio aproximado de 5 kilómetros
                             
-                            Intentalo en otro momento o contacta con los que me programaron 
+                            Inténtalo en otro momento o contacta con los que me programaron 
                             @RaulPadillaDelgado (https://raulpadilladelgado.github.io/)
-                            Isaac Aiman ()
-                   """.trimIndent()
-        }
-        fun chosenGasType(actualGasType: String): String {
-            return """
-                        Quieres echar $actualGasType. Ahora manda tu ubicación y te buscaré las tres más baratas cerca de ti
+                            iaimans (iaimans@protonmail.com)
                    """.trimIndent()
         }
 
         fun showingGasStations(gasType: GasType): String {
             return """
-                        Mostrando las gasolineras más baratas a una distancia máxima de 5 kilómetros para el combustible elegido (${gasType.printableName})
+                        Mostrando las gasolineras más baratas en un radio aproximado de 5 kilómetros para el combustible elegido (${gasType.printableName})
                    """.trimIndent()
         }
 
-        fun chosenDistance(actualDistance: Int): String {
-            return """
-                        Se ha establecido $actualDistance kilómetros como la distancia máxima aproximada para la búsqueda de gasolineras
-                   """.trimIndent()
+        fun findCheapestGasStationsButtonText(): String {
+            return "📍 Compartir ubicación"
         }
 
-        fun findCheapestGasStationsButtonText() :String {
-            return "\uD83D\uDD0E Buscar gasolineras baratas"
-        }
-
-        fun changeGasTypeButtonText() :String {
-            return "⛽ Cambiar combustible"
-        }
-
-        fun changeMaximumDistanceButtonText() :String {
-            return "\uD83D\uDEE3️ Cambiar distancia"
-        }
-
-        fun listOfGasType(): String {
-            return """
-        Gasolina 95 E5 -> /gasolina95e5,
-        Gasolina 95 E5 Premium -> /gasolina95e5premium,
-        Gasolina 95 E10 -> /gasolina95e10,
-        Gasolina 98 E5 -> /gasolina98e5,
-        Gasolina 98 E10 -> /gasolina98e10,
-        Gasoil A -> /gasoila,
-        Gasoil B -> /gasoilb,
-        Gasoil Premium -> /gasoilpremium,
-                    """.trimIndent()
+        fun chooseGas(): String {
+            return "⛽ ️Elige el combustible deseado:"
         }
     }
 }
