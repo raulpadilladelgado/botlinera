68470d99392a0963962691ee1ebe2def7112ec27
Raúl Padilla Delgado <padilladelgadoraul@gmail.com>
2022-10-11 13:01:20 +0100

    PoC-check_dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.7.10"
     id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.ben-manes.versions") version "0.25.0"
 
     application
 }
